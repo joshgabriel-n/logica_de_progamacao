4e18566795d506883427baace827c32ba7068ad1
vitor maues <joaomaues11@gmail.com>
2025-06-13 11:47:34 -0400

    feat home

---
+++ Lacos/Switch_Case.js
@@ -0,0 +1,23 @@
+var dia = "quarta";
+switch (dia){
+  case "segunda":
+  console.log("Dia de começar a semana");
+  break
+
+ case "terça":
+  console.log("Hora de aprender");
+  break;
+
+   case "Quarta":
+  console.log("Metade da semana, continue firme");
+  break;
+
+   case "Quinta":
+  console.log("Quase lá, mais um pouco de esforço");
+  break;
+
+   case "Sexta":
+  console.log("Dia de coneirar, o fim de semana chegou");
+  break;
+  default:  console.log("Final de seena chegou");
+}
--- Lacos/lacos.js
+++ Lacos/lacos.js
@@ -1,5 +1,5 @@
-Let numero;
+var numero;
 do {
-    numero = prompt('Digite um numero 1 e 10:');
+    numero = prompt("Digite um numero 1 e 10:");
 } while(numero < 1 || numero > 10);
-console.log(`Você digitou o numero $ {numero}.`);
+console.log(`Você digitou o numero ${numero}.`);
