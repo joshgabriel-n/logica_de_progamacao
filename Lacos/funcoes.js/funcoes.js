1fafecd17573ace8d6253afd7dd62237adc69660
vitor maues <joaomaues11@gmail.com>
2025-06-16 11:35:40 -0400

    feat drop psuh

--- Lacos/funcoes.js/funcoes.js
+++ Lacos/funcoes.js/funcoes.js
@@ -1,12 +1,19 @@
-/*function dobrar(num){valorm = num *2;
+function dobrar(num){valorm = num *2;
     return num;
 }
 let valor = 10;
 console.log(dobrar (valor)); // 20
-console.log(valor); // (valor origial não mudou)*/    
+console.log(valor); // (valor origial não mudou)    
 
 function saudacao(nome="Visitante") {
     console.log(`Ola, ${nome}`)
 }
 saudacao();//
 saudacao("joshua"); //
+
+function adicionarItem(lista) {
+    lista.push("Novo item");
+}
+let itens=["Item 1" ,"Item 2"];
+adicionarItem(itens);
+console.log(itens);//["Item 1","Item 2", "Novo item"]
