b4cf3f7c57679a921e8249bb7d4e95ac433db07f
vitor maues <joaomaues11@gmail.com>
2025-06-16 11:13:02 -0400

    feat drop push

--- Lacos/funcoes.js/funcoes.js
+++ Lacos/funcoes.js/funcoes.js
@@ -1,6 +1,12 @@
-function dobrar(num){valorm = num *2;
+/*function dobrar(num){valorm = num *2;
     return num;
 }
 let valor = 10;
 console.log(dobrar (valor)); // 20
-console.log(valor); // (valor origial não mudou)    
+console.log(valor); // (valor origial não mudou)*/    
+
+function saudacao(nome="Visitante") {
+    console.log(`Ola, ${nome}`)
+}
+saudacao();//
+saudacao("joshua"); //
