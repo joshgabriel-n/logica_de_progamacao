a858173e8805bd0b175d90dc202be474f1c87fb8
vitor maues <joaomaues11@gmail.com>
2025-07-03 11:53:16 -0400

    projeto feat progamação em c

---
+++ progamção em c/inicial.cpp
@@ -0,0 +1,12 @@
+#inclube <stdio.h>
+
+int main ()
+{
+int idade_dog= 4;
+char inicial_nome; 'B';
+char nome_completo[50] ; ('Macanudo');
+float peso;12,300;
+double altura;0,45 metros');
+printf ("a idade do dog é %\n",idade_dog);
+printf ("a inicial do dog é %\n", inicial_dog);
+}
